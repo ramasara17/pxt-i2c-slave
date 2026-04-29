e76df315a2f3f29d333214fdb2dc9533b7138f7d
ramasara17 <suresh@cytron.io>
2026-04-29 17:07:49 +0800

    Update i2cslave.ts

--- i2cslave.ts
+++ i2cslave.ts
@@ -1,44 +1,13 @@
-//% color="#AA278D" weight=100 icon="\uf085"
-//% block="I2C Slave"
-namespace i2cSlave {
+declare namespace i2cSlave {
+    //% shim=i2cSlave::initSlave
+    function initSlave(addr: number): void;
 
-    /**
-     * Initialise I2C slave at given address
-     * @param addr I2C address e.g. 117 for 0x75
-     */
-    //% blockId="i2cSlave_init"
-    //% block="init I2C slave at address %addr"
-    //% addr.defl=117
-    export function initSlave(addr: number): void {
-        return;
-    }
+    //% shim=i2cSlave::dataAvailable
+    function dataAvailable(): boolean;
 
-    /**
-     * Returns true if new data received from master
-     */
-    //% blockId="i2cSlave_dataAvailable"
-    //% block="I2C slave data available"
-    export function dataAvailable(): boolean {
-        return false;
-    }
+    //% shim=i2cSlave::readByte
+    function readByte(): number;
 
-    /**
-     * Read last received byte from master
-     */
-    //% blockId="i2cSlave_readByte"
-    //% block="I2C slave read byte"
-    export function readByte(): number {
-        return 0;
-    }
-
-    /**
-     * Write response byte to master
-     * @param val byte value to send
-     */
-    //% blockId="i2cSlave_writeByte"
-    //% block="I2C slave write byte %val"
-    //% val.defl=0
-    export function writeByte(val: number): void {
-        return;
-    }
+    //% shim=i2cSlave::writeByte
+    function writeByte(val: number): void;
 }
